1919ed92b25cf255ba7c995688407b0dc28915db
Romain Beguet <beguet@adacore.com>
2022-06-19 16:13:49 +0200

    Implement conflict analysis and backjumping.

--- src/solver-dpll.adb
+++ src/solver-dpll.adb
@@ -3,12 +3,22 @@ with Ada.Unchecked_Deallocation;
 
 package body Solver.DPLL is
    type Decision_Array is array (Positive range <>) of Natural;
+   type Literal_Mask is array (Integer range <>) of Boolean;
+
+   type Formula_Access is access Formula;
+
+   procedure Free is new Ada.Unchecked_Deallocation
+     (Formula, Formula_Access);
+
+   procedure Free is new Ada.Unchecked_Deallocation
+     (Literal_Array, Literal_Options);
 
    function Unassigned_Count (M : Model) return Natural;
 
    function First_Unassigned (M : Model) return Natural;
 
-   function Solve_No_Theory (F : Formula; M : in out Model) return Boolean;
+   function Solve_No_Theory
+     (F : in out Formula_Access; M : in out Model) return Boolean;
 
    function Unassigned_Count (M : Model) return Natural
    is
@@ -32,21 +42,38 @@ package body Solver.DPLL is
       return 0;
    end First_Unassigned;
 
-   function Solve_No_Theory (F : Formula; M : in out Model) return Boolean is
-      Unassigned_Left : Natural := Unassigned_Count (M);
-      Lit_Decisions   : Decision_Array := (1 .. Unassigned_Left => 0);
-      Decision_Level  : Natural := 0;
+   function Solve_No_Theory
+     (F : in out Formula_Access; M : in out Model) return Boolean
+   is
+      Unassigned_Left    : Natural := Unassigned_Count (M);
+      Lit_Decisions      : Decision_Array := (1 .. Unassigned_Left => 0);
+      Lit_Antecedants    : Decision_Array := (1 .. Unassigned_Left => 0);
+      Decision_Level     : Natural := 0;
+      Conflicting_Clause : Natural := 0;
 
-      procedure Assign (Lit_Index : Natural; Value : Boolean);
+      procedure Assign
+        (Lit_Index : Natural; Value : Boolean; Antecedant : Natural);
+
       procedure Unassign (Lit_Index : Natural);
+
       function Unit_Propagate return Boolean;
+
       function Backtrack return Boolean;
+      function Backjump return Boolean;
+
+      function Resolve
+        (Left, Right : Literal_Options;
+         Pivot_Lit   : Natural) return Literal_Options;
+
       procedure Decide;
 
-      procedure Assign (Lit_Index : Natural; Value : Boolean) is
+      procedure Assign
+        (Lit_Index : Natural; Value : Boolean; Antecedant : Natural)
+      is
       begin
          M (Lit_Index) := (if Value then True else False);
          Lit_Decisions (Lit_Index) := Decision_Level;
+         Lit_Antecedants (Lit_Index) := Antecedant;
          Unassigned_Left := Unassigned_Left - 1;
       end Assign;
 
@@ -63,8 +90,9 @@ package body Solver.DPLL is
          while Unit_Clause_Found loop
             Unit_Clause_Found := False;
 
-            for Dis of F loop
+            for C in F.all'Range loop
                declare
+                  Dis         : constant Literal_Options := F.all (C);
                   Unset_Count : Natural := 0;
                   Last_Unset  : Integer;
                   Is_Sat      : Boolean := False;
@@ -91,9 +119,10 @@ package body Solver.DPLL is
                      null;  -- continue
                   elsif Unset_Count = 1 then
                      Put_Line ("Propagate");
-                     Assign (abs Last_Unset, Last_Unset > 0);
+                     Assign (abs Last_Unset, Last_Unset > 0, C);
                      Unit_Clause_Found := True;
                   elsif Unset_Count = 0 then
+                     Conflicting_Clause := C;
                      return False;
                   end if;
                end;
@@ -121,16 +150,119 @@ package body Solver.DPLL is
                Unassign (Index);
             end if;
          end loop;
-         Assign (First, (if Value in True then False else True));
+         Put_Line ("Redecide " & First'Image);
+         Assign (First, (if Value in True then False else True), 0);
          return True;
       end Backtrack;
+
+      function Backjump return Boolean is
+         Found             : Natural := 0;
+         Pivot             : Natural := 0;
+         Learnt_Clause     : Literal_Options :=
+            new Literal_Array'(F (Conflicting_Clause).all);
+      begin
+         if Decision_Level <= 0 then
+            return False;
+         end if;
+
+         Put_Line ("Backjump");
+         Put_Line ("Conflicting clause :" & Conflicting_Clause'Image);
+         while True loop
+            Found := 0;
+
+            for Lit of Learnt_Clause.all loop
+               Put_Line
+                 ("Antecedant of " & Lit'Image & " is "
+                  & Lit_Antecedants (abs Lit)'Image);
+               if Lit_Decisions (abs Lit) = Decision_Level then
+                  Found := Found + 1;
+                  if Lit_Antecedants (abs Lit) /= 0 then
+                     Pivot := abs Lit;
+                  end if;
+               end if;
+            end loop;
+
+            if Found = 1 then
+               exit;
+            end if;
+
+            declare
+               Old_Learnt_Clause : Literal_Options := Learnt_Clause;
+            begin
+               Learnt_Clause := Resolve
+                 (Learnt_Clause, F (Lit_Antecedants (Pivot)), Pivot);
+               Free (Old_Learnt_Clause);
+            end;
+         end loop;
+         Put ("Learnt clause : ");
+         for Lit of Learnt_Clause.all loop
+            Put (Lit'Image);
+            Put (" ");
+         end loop;
+         New_Line;
+
+         declare
+            Backjump_Decision_Level : Natural := 0;
+            Lit_Decision_Level      : Natural := 0;
+         begin
+            for Lit of Learnt_Clause.all loop
+               Lit_Decision_Level := Lit_Decisions (abs Lit);
+               if Lit_Decision_Level /= Decision_Level and then
+                  Lit_Decision_Level > Backjump_Decision_Level
+               then
+                  Backjump_Decision_Level := Lit_Decision_Level;
+               end if;
+            end loop;
+            Decision_Level := Backjump_Decision_Level;
+         end;
+
+         for Lit in Lit_Decisions'Range loop
+            if Lit_Decisions (Lit) > Decision_Level then
+               Unassign (Lit);
+            end if;
+         end loop;
+
+         declare
+            Old_Formula : Formula_Access := F;
+         begin
+            F := new Formula'(F.all & Learnt_Clause);
+            Free (Old_Formula);
+         end;
+
+         Put_Line ("Backjumping to level :" & Decision_Level'Image);
+         return True;
+      end Backjump;
+
+      function Resolve
+        (Left, Right : Literal_Options;
+         Pivot_Lit   : Natural) return Literal_Options
+      is
+         New_Clause : Literal_Array := Left.all & Right.all;
+         Index : Natural := 1;
+         Last  : Natural := New_Clause'Last;
+         Seen  : Literal_Mask (-M'Last .. M'Last) := (others => False);
+      begin
+         while Index <= Last loop
+            if abs New_Clause (Index) = Pivot_Lit then
+               New_Clause (Index) := New_Clause (Last);
+               Last := Last - 1;
+            elsif Seen (New_Clause (Index)) then
+               New_Clause (Index) := New_Clause (Last);
+               Last := Last - 1;
+            else
+               Seen (New_Clause (Index)) := True;
+               Index := Index + 1;
+            end if;
+         end loop;
+         return new Literal_Array'(New_Clause (1 .. Last));
+      end Resolve;
 
       procedure Decide is
          Index : constant Natural := First_Unassigned (M);
       begin
-         Put_Line ("Decide");
+         Put_Line ("Decide " & Index'Image);
          Decision_Level := Decision_Level + 1;
-         Assign (Index, True);
+         Assign (Index, True, 0);
       end Decide;
    begin
       if not Unit_Propagate then
@@ -141,7 +273,7 @@ package body Solver.DPLL is
          while True loop
             if Unit_Propagate then
                exit;
-            elsif not Backtrack then
+            elsif not Backjump then
                return False;
             end if;
          end loop;
@@ -149,15 +281,11 @@ package body Solver.DPLL is
       return True;
    end Solve_No_Theory;
 
-   type Formula_Access is access Formula;
-
-   procedure Free is new Ada.Unchecked_Deallocation (Formula, Formula_Access);
-
    function Solve (F : Formula; M : in out Model) return Boolean is
       Orig_Model : constant Model := M;
       Updated    : Formula_Access := new Formula'(F);
    begin
-      while Solve_No_Theory (Updated.all, M) loop
+      while Solve_No_Theory (Updated, M) loop
          declare
             OK          : Boolean;
             Old_Formula : constant Formula := Updated.all;
